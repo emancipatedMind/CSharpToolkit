d80aabfbc0370ca6448a066190ab6b1e0de1b1e0
emancipatedMind <powensfinch@gmail.com>
2016-07-19 14:15:03 -0400

    Updated to include BaseEntity class.

---
+++ EntityBase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToolkit {
+    public abstract class EntityBase : INotifyDataErrorInfo, INotifyPropertyChanged {
+        protected readonly Dictionary<string, List<string>> _errors = new Dictionary<string, List<string>>();
+
+        public IEnumerable GetErrors(string propertyName) {
+            if (string.IsNullOrEmpty(propertyName))
+                return _errors.Values;
+            return _errors.ContainsKey(propertyName) ? _errors[propertyName] : null;
+        }
+
+        public bool HasErrors => _errors.Count != 0;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        protected void OnErrorsChanged(string propertyName) {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        protected void ClearErrors(string propertyName = "") {
+            _errors.Remove(propertyName);
+            OnErrorsChanged(propertyName);
+        }
+
+        protected void AddError(string propertyName, string error) {
+            AddErrors(propertyName, new List<string> { error });
+        }
+
+        protected void AddErrors(string propertyName, IList<string> errors) {
+            bool changed = false;
+            if (!_errors.ContainsKey(propertyName)) {
+                _errors.Add(propertyName, new List<string>());
+                changed = true;
+            }
+            errors.ToList().ForEach(x => {
+                if (_errors[propertyName].Contains(x)) return;
+                _errors[propertyName].Add(x);
+                changed = true;
+            });
+            if (changed)
+                OnErrorsChanged(propertyName);
+        }  
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChange([CallerMemberName] string propertyName = "" ) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); 
+        }
+    }
+}
