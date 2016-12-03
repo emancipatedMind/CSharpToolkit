493e52fea0ab85b7b491c6107820606ef496d240
emancipatedMind <powensfinch@gmail.com>
2016-12-02 23:38:09 -0500

    Added playground to solution. Restructured solution as well.

---
+++ CSharpToolkitPlayground/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharpToolkitPlayground {
+    using static System.Console;
+    using CSharpToolkit.Console;
+
+    class Program {
+        static void Main(string[] args) {
+
+            string title = "Bar Drawer By Percentage Test";
+            WriteLine($"***** {title} *****\n");
+
+            var drawer = new BarDrawerByPercentage();
+
+            int[] seeds = { 9, 39 };
+
+            foreach(int seed in seeds) {
+                drawer.Seed = seed;
+                for (int j = 0; j < 8; j++) {
+                    for (int i = 0; i < drawer.Seed; i++) {
+                        drawer.UpdateBar();
+                        System.Threading.Tasks.Task.Delay(30).Wait(); 
+                    }
+                    WriteLine();
+                }
+            } 
+
+            ReadLine();
+        }
+    }
+}
