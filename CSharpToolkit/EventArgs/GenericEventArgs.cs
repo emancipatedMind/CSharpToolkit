bb63ee82ed00be92da1bbc72be384ad44c30d0e9
emancipatedMind <powensfinch@gmail.com>
2017-10-20 10:47:20 -0400

    Added a few classes to toolkit.

---
+++ CSharpToolkit/EventArgs/GenericEventArgs.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharpToolkit.EventArgs {
+    public class GenericEventArgs<T> : System.EventArgs {
+        public T Data { get; private set; }
+        public GenericEventArgs(T data) {
+            Data = data;
+        }
+    }
+}
