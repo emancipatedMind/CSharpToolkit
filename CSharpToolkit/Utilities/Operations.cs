0d4c949d546da792301c38871e4c32c4400437ae
emancipatedMind <powensfinch@gmail.com>
2017-11-03 08:13:49 -0400

    Added new namespace to Toolkit called Utilities with a class that can withdraw a string from a SecureString class as its first method.

---
+++ CSharpToolkit/Utilities/Operations.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharpToolkit.Utilities {
+    using Common;
+    using System;
+    using System.Security;
+    using System.Runtime.InteropServices;
+    public static class Operations {
+        public static OperationResult<string> DecodeSecureString(SecureString value) {
+            IntPtr valuePtr = IntPtr.Zero;
+            try {
+                valuePtr = Marshal.SecureStringToGlobalAllocUnicode(value);
+                string plainSecureString = Marshal.PtrToStringUni(valuePtr);
+                return new OperationResult<string>(plainSecureString);
+            }
+            catch (Exception ex) {
+                return new OperationResult<string>(new Exception[] { ex });
+            }
+            finally {
+                Marshal.ZeroFreeGlobalAllocUnicode(valuePtr);
+            }
+        }
+    }
+}
