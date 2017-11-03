322671c520c81bb62baefc4ccd45bde6cc1a178e
emancipatedMind <powensfinch@gmail.com>
2017-11-03 13:32:31 -0400

    Added a bunch of tests for new method used to replace variables.

--- CSharpToolkitTester/OperationsTester.cs
+++ CSharpToolkitTester/OperationsTester.cs
@@ -3,9 +3,9 @@
     using System.Security;
     using System.Linq;
     using CSharpToolkit.Utilities;
-    [TestFixture] 
+    [TestFixture]
     public class OperationsTester {
-        [Test] 
+        [Test]
         public void DecodeSecureStringShouldMatchStringPassedIn() {
             string expected = "123abczyx";
             SecureString secureString = new SecureString();
@@ -20,6 +20,120 @@
             Assert.IsTrue(result.WasSuccessful);
             Assert.IsFalse(result.HadErrors);
             Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentReturnsTrueIfValuesAreDifferentForValueType() {
+            int oldValue = 5;
+            int newValue = 6;
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+
+            Assert.IsTrue(result.WasSuccessful);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentShouldReplaceIfValueTypesDifferent() {
+            int oldValue = 5;
+            int expected = 6;
+            int newValue = expected;
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+
+            Assert.AreEqual(expected, oldValue);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentShouldReplaceIfReferenceTypesAreDifferentInstances() {
+            object oldValue = new object();
+            object expected = new object();
+            object newValue = expected;
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+
+            Assert.AreSame(expected, oldValue);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentShouldLeaveVariableWithValueIfValueTypeAndSame() {
+            int oldValue = 5;
+            int expected = 5;
+            int newValue = expected;
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+
+            Assert.AreEqual(expected, oldValue);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentShouldLeaveVariableWithObjectIfReferenceTypeAndSame() {
+            object expected = new object();
+            object oldValue = expected;
+            object newValue = expected;
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+
+            Assert.AreSame(expected, oldValue);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentReturnsTrueIfReferenceTypesAreDifferentInstances() {
+            object oldValue = new object();
+            object newValue = new object();
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+
+            Assert.IsTrue(result.WasSuccessful);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentReturnsFalseIfOldValueAndDefaultAreSameAndNewValueIsNull() {
+            object oldValue = new object();
+            object defaultValue = oldValue;
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, null, defaultValue);
+
+            Assert.IsFalse(result.WasSuccessful);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentReturnsTrueIfOldValueAndDefaultAreDifferentAndNewValueIsNull() {
+            object oldValue = new object();
+            object defaultValue = new object();
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, null, defaultValue);
+
+            Assert.IsTrue(result.WasSuccessful);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentReturnsFalseIfValuesAreSameForValueType() {
+            int oldValue = 5;
+            int newValue = 5;
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+
+            Assert.IsFalse(result.WasSuccessful);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentReturnsFalseIfReferenceTypesAreSameInstance() {
+            object oldValue = new object();
+            object newValue = oldValue;
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+
+            Assert.IsFalse(result.WasSuccessful);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentReturnsFalseIfOldValueIsNull() {
+            object oldValue = null;
+            object newValue = new object();
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+
+            Assert.IsFalse(result.WasSuccessful);
+        }
+        [Test]
+        public void PerformReplaceIfDifferentReturnsFalseIfNewValueAndDefaultValueAreNull() {
+            object oldValue = new object();
+            object newValue = null;
+
+            OperationResult result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue);
+            Assert.IsFalse(result.WasSuccessful);
+
+            result = Operations.PerformReplaceIfDifferent(ref oldValue, newValue, null);
+            Assert.IsFalse(result.WasSuccessful);
         }
     }
 }
