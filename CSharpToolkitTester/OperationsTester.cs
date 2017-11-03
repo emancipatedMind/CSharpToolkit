500b718a5c3f85c74e54be59450c061739a13fa8
emancipatedMind <powensfinch@gmail.com>
2017-11-03 08:18:32 -0400

    Added new project to test some of the functionality located in CSharpToolkit. The first tester tests recently added CSharpToolkit.Utilities.Operations.DecodeSecureString method.

---
+++ CSharpToolkitTester/OperationsTester.cs
@@ -0,0 +1,25 @@
+﻿namespace CSharpToolkitTester {
+    using NUnit.Framework;
+    using System.Security;
+    using System.Linq;
+    using CSharpToolkit.Utilities;
+    [TestFixture] 
+    public class OperationsTester {
+        [Test] 
+        public void DecodeSecureStringShouldMatchStringPassedIn() {
+            string expected = "123abczyx";
+            SecureString secureString = new SecureString();
+
+            expected
+                .ToList()
+                .ForEach(c => secureString.AppendChar(c));
+
+            var result = Operations.DecodeSecureString(secureString);
+            string actual = result.Result;
+
+            Assert.IsTrue(result.WasSuccessful);
+            Assert.IsFalse(result.HadErrors);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
