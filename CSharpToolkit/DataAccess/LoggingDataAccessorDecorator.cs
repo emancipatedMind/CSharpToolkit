c47f283840225f13a605189dadad4d4ff2af6467
emancipatedMind <powensfinch@gmail.com>
2017-11-29 15:37:13 -0500

    Created a duration logger which will log the duration of an operation.

--- CSharpToolkit/DataAccess/LoggingDataAccessorDecorator.cs
+++ CSharpToolkit/DataAccess/LoggingDataAccessorDecorator.cs
@@ -5,26 +5,18 @@
     using Abstractions.Logging;
     public class LoggingDataAccessorDecorator : IDataReaderAccessor {
 
-        ITimeStampLogger _logger;
+        IDurationLogger _logger;
         string _separationString = " : " ;
         IDataReaderAccessor _component;
 
-        public LoggingDataAccessorDecorator(IDataReaderAccessor component, ITimeStampLogger logger) {
+        public LoggingDataAccessorDecorator(IDataReaderAccessor component, IDurationLogger logger) {
             _logger = logger;
             _component = component;
         }
 
         public void UseDataReader(string sql, Action<IDataReader> callback) {
-            DateTime beginTime = LogWithCurrentTime("Operation has begun.\r\n", false);
-            _logger.Log(sql + "\r\n");
-            _component.UseDataReader(sql, callback);
-            DateTime finishTime = LogWithCurrentTime("Operation Done.\r\n", false);
-            LogWithCurrentTime($"Operation Duration : {(finishTime - beginTime).TotalMilliseconds} ms.\r\n", false);
-            _logger.Log("\r\n");
+            _logger.LogTimeMetricsOf(() => _component.UseDataReader(sql, callback));
         }
-
-        DateTime LogWithCurrentTime(string content, bool prependNewLine) =>
-            _logger.LogWithCurrentTime(content, _separationString, prependNewLine);
 
     }
 }
---
+++ CSharpToolkit/Logging/DurationLogger.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharpToolkit.Logging {
+    using System;
+    using System.Data;
+    using Abstractions.DataAccess;
+    using Abstractions.Logging;
+    using EventArgs;
+
+    public class DurationLogger : IDurationLogger {
+
+        ITimeStampLogger _logger;
+        string _separationString = " : " ;
+
+        public DurationLogger(ITimeStampLogger logger) {
+            _logger = logger;
+            _logger.LogOutputFailure += (s, e) => LogOutputFailure?.Invoke(this, e);
+        }
+
+        public bool LoggerFaulted => _logger.LoggerFaulted;
+
+        public event EventHandler<GenericEventArgs<Exception>> LogOutputFailure;
+
+        public void Log(string content) => _logger.Log(content);
+
+        public void LogTimeMetricsOf(Action operation) {
+            DateTime beginTime = LogWithCurrentTime("Operation has begun.\r\n", false);
+            operation();
+            DateTime finishTime = LogWithCurrentTime("Operation Done.\r\n", false);
+            LogWithCurrentTime($"Operation Duration : {(finishTime - beginTime).TotalMilliseconds} ms.\r\n", false);
+            _logger.Log("\r\n");
+        }
+
+        DateTime LogWithCurrentTime(string content, bool prependNewLine) =>
+            _logger.LogWithCurrentTime(content, _separationString, prependNewLine);
+
+    }
+}
