01bf4e1f89c4da71a2ed23967f54964eb19e4669
emancipatedMind <powensfinch@gmail.com>
2017-12-01 18:30:35 -0500

    Updated LoggingDataAccessor.

---
+++ CSharpToolkit/DataAccess/LoggingDataAccessor.cs
@@ -0,0 +1,27 @@
+﻿namespace CSharpToolkit.DataAccess {
+    using System;
+    using System.Data;
+    using Abstractions.DataAccess;
+    using Abstractions.Logging;
+    public class LoggingDataAccessor : IDataReaderAccessor {
+
+        IDurationLogger _logger;
+        string _separationString;
+        IDataReaderAccessor _component;
+
+        public LoggingDataAccessor(IDataReaderAccessor component, IDurationLogger logger) : this(component, logger, " : ") { }
+        public LoggingDataAccessor(IDataReaderAccessor component, IDurationLogger logger, string separationString) {
+            _separationString = separationString;
+            _logger = logger;
+            _component = component;
+        }
+
+        public void UseDataReader(string sql, Action<IDataReader> callback) {
+            _logger.LogTimeMetricsOf(() => {
+                _logger.Log(sql);
+                _component.UseDataReader(sql, callback);
+            });
+        }
+
+    }
+}
