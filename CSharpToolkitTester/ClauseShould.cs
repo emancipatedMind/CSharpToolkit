ebcde93773246245945426a935318fd41c768e42
emancipatedMind <powensfinch@gmail.com>
2018-01-16 10:20:35 -0500

    Working on adding tests.

---
+++ CSharpToolkitTester/ClauseShould.cs
@@ -0,0 +1,35 @@
+﻿namespace CSharpToolkitTester {
+    using CSharpToolkit.DataAccess;
+    using NUnit.Framework;
+    using System.Linq;
+    [TestFixture]
+    public class ClauseShould {
+
+        [Test]
+        public void ReturnEmptyStringIfNoClauseAdded() {
+            System.Enum
+                .GetValues(typeof(ClauseType))
+                .Cast<ClauseType>()
+                .ToList()
+                .ForEach(clauseType => Assert.AreEqual(string.Empty, Clause.New(clauseType).Build().Query));
+        }
+
+        [Test]
+        public void NotSurroundSingleORClauseWithparanthesesAndShouldHaveAPrecedingWhiteSpace() {
+            var query = Clause.New(ClauseType.OR).AddClause("OrderId = 187").Build().Query;
+            Assert.AreEqual(" OrderId = 187", query);
+        }
+
+        [Test]
+        public void SurroundMultipleORClauseWithparanthesesShouldHaveAPrecedingWhiteSpaceAndCombinedWithOR() {
+            var query = Clause.New(ClauseType.OR).AddClause("OrderId = 187").AddClause("CustomerId = 985").Build().Query;
+            Assert.AreEqual(" (OrderId = 187 OR CustomerId = 985)", query);
+        }
+
+        [Test]
+        public void MultipleANDClauseHasNoParanthesesShouldHaveAPrecedingWhiteSpaceAndCombinedWithAND() {
+            var query = Clause.New(ClauseType.AND).AddClause("OrderId = 187").AddClause("CustomerId = 985").Build().Query;
+            Assert.AreEqual(" OrderId = 187 AND CustomerId = 985", query);
+        }
+    }
+}
